f9436525386c5bb280a74ac75e5db7b32777913c
Magnus Nielsen <magnus@magnus-K61IC.(none)>
2012-12-06 22:51:42 +0100

    Something is wrong with the indentations I think, getting 'unknown variable flyTo'. I THINK it works otherwise

--- uppgift8.py
+++ uppgift8.py
@@ -21,11 +21,6 @@ parser.add_option ("-g", "--group", action="store", type="int",
 #
 
 class myai:
-    """Simple Stub for a Bot"""
-
-    def __init__(self):
-        self.count = 0
-        self.mode = "init"
 
     def check_wall (self, dist):
 #        print ("CHECKWALL")
@@ -36,19 +31,22 @@ class myai:
             direction = heading
             if vx != 0 or vy != 0:
                 direction = math.degrees (math.atan2 (vy, vx))
-
-                distance_to_wall = ai.wallFeeler (dist, int(direction), 1, 1)  
-                # ai.wallFeller return dist if there is no wall in the direction
-                # and nearer than dist. If there is a wall nearer than dist it
-                # returns the actual distance to the wall.
-
-                return dist != distance_to_wall
-
+            
+            distance_to_wall = ai.wallFeeler (dist, int(direction), 1, 1)  
+            # ai.wallFeller return dist if there is no wall in the direction
+            # and nearer than dist. If there is a wall nearer than dist it
+            # returns the actual distance to the wall.
+            
+            return dist != distance_to_wall
         except:
             e = sys.exc_info()
             print ("ERROR check_wall: ", e)
 
-        return False
+            return False
+
+    def __init__(self):
+        self.count = 0
+        self.mode = "init"
 
     def tick(self):
         try:
@@ -68,7 +66,7 @@ class myai:
             #
 
             heading = ai.selfHeadingDeg() 
-            # 0-360, 0 in x direction, positive toward y
+        
             ai.setTurnSpeed(64.0)
             speed = ai.selfSpeed()
             x = ai.selfX()
@@ -76,8 +74,9 @@ class myai:
             vx = ai.selfVelX()
             vy = ai.selfVelY()
             # Add more sensors readings here if they are needed
+            self.wanted_heading = flyTo(ai.closestRadarX(), ai.closestRadarY(),ai.selfRadarX(),ai.selfRadarY())
 
-            print (self.mode, ai.shotVel(0), ai.shotVelDir(0))
+            
 
 
             # avoid strange sensor values when starting by waiting
@@ -90,20 +89,24 @@ class myai:
                     self.mode = "moving"
                 elif ai.closestShipId() != -1:
                     self.mode = "shooting"
-            
-            elif self.mode == "moving":
-                flyTo(ai.closestRadarX(), ai.closestRadarY(),ai.selfRadarX(),ai.selfRadarY())
-                avoidAngle = turnAngle(heading)
-                if self.check_wall(200):
-                    self.mode = "turning"
+                
+                elif self.mode == "moving":
+                
+                    if ai.selfSpeed() < 2:
+                        ai.thrust(1)
+                    elif a.selfSpeed() >= 2:
+                        ai.thrust(0)
+                    elif self.check_wall(200):
+                        self.mode = "turning"
+                    elif ai.closestShipId() != -1:
+                        self.mode = "shooting"
             
             elif self.mode == "turning":
-                ai.turn(avoidAngle)
-                if abs (ai.angleDiff (int(heading), int(avoidAngle))) < 2:
+                self.wanted_heading += 180
+                self.wanted_heading %= 360
+                ai.thrust(0)
+                if abs (ai.angleDiff (int(heading), int(self.wanted_heading))) < 2:
                     self.mode = "waitnowall"
-                    
-                if ai.closestShipId() != -1:
-                    self.mode = "shooting"
                     
             # Wait until wanted heading is achieved. Then go to state waitnowall.
             
@@ -115,6 +118,7 @@ class myai:
 
 
             elif self.mode == "shooting":
+                ai.thrust(0)
                 if ai.closestShipId() == -1:
                     self.mode = "ready"
                 else:
@@ -130,86 +134,71 @@ class myai:
             print ("ERROR: ", e)
 
    
-def shoot(id):
-    selfX=ai.selfX()
-    selfY=ai.selfY()
-    selfVelocity=ai.selfSpeed()
-    selfTracking=ai.selfTrackingRad()
-    if math.isnan(selfTracking):
-        selfTracking=0
-    selfVelocityX=selfVelocity*math.cos(selfTracking)
-    selfVelocityY=selfVelocity*math.sin(selfTracking)
+    def shoot(id):
+        selfX=ai.selfX()
+        selfY=ai.selfY()
+        selfVelocity=ai.selfSpeed()
+        selfTracking=ai.selfTrackingRad()
+        if math.isnan(selfTracking):
+            selfTracking=0
+        selfVelocityX=selfVelocity*math.cos(selfTracking)
+        selfVelocityY=selfVelocity*math.sin(selfTracking)
+
+        enemyX=ai.screenEnemyXId(id)
+        enemyY=ai.screenEnemyYId(id)
+        enemyVelocity=ai.enemySpeedId(id)
+        enemyTracking=ai.enemyTrackingRadId(id)
+        if math.isnan(enemyTracking):
+            enemyTracking=0
+        enemyVelocityX=enemyVelocity*math.cos(enemyTracking)
+        enemyVelocityY=enemyVelocity*math.sin(enemyTracking)
+        bulletVelocity=10 #emptybordernofriction.xp
+
+        relativeX=enemyX-selfX
+        relativeY=enemyY-selfY
+        relativeVelocityX=enemyVelocityX-selfVelocityX
+        relativeVelocityY=enemyVelocityY-selfVelocityY
 
 
+        time=timeOfImpact(relativeX, relativeY, relativeVelocityX, relativeVelocityY, bulletVelocity)
 
-    enemyX=ai.screenEnemyXId(id)
-    enemyY=ai.screenEnemyYId(id)
-    enemyVelocity=ai.enemySpeedId(id)
-    enemyTracking=ai.enemyTrackingRadId(id)
-    if math.isnan(enemyTracking):
-        enemyTracking=0
-    enemyVelocityX=enemyVelocity*math.cos(enemyTracking)
-    enemyVelocityY=enemyVelocity*math.sin(enemyTracking)
-    bulletVelocity=10 #emptybordernofriction.xp
+        targetX=enemyX+enemyVelocityX*time
+        targetY=enemyY+enemyVelocityY*time
+        targetAngle=math.atan2(targetY-selfY,targetX-selfX)
+        targetAngle=ai.radToDeg(targetAngle)
+        egenAngle=int(ai.selfHeadingDeg())
+        diffAngle=ai.angleDiff(egenAngle,targetAngle)
 
-    relativeX=enemyX-selfX
-    relativeY=enemyY-selfY
-    relativeVelocityX=enemyVelocityX-selfVelocityX
-    relativeVelocityY=enemyVelocityY-selfVelocityY
+        ai.turn(diffAngle)
+        return diffAngle
 
+    def timeOfImpact(relativeX, relativeY, targetSpeedX, targetSpeedY, bulletSpeed): #inspired by http://playtechs.blogspot.se/2007/04/aiming-at-moving-target.html
 
-    time=timeOfImpact(relativeX, relativeY, relativeVelocityX, relativeVelocityY, bulletVelocity)
+        a=bulletSpeed * bulletSpeed - (targetSpeedX*targetSpeedX+targetSpeedY*targetSpeedY)
+        b=relativeX*targetSpeedX+relativeY*targetSpeedY
+        c=relativeX*relativeX+relativeY*relativeY
 
-    targetX=enemyX+enemyVelocityX*time
-    targetY=enemyY+enemyVelocityY*time
-    targetAngle=math.atan2(targetY-selfY,targetX-selfX)
-    targetAngle=ai.radToDeg(targetAngle)
-    egenAngle=int(ai.selfHeadingDeg())
-    diffAngle=ai.angleDiff(egenAngle,targetAngle)
+        d=b*b+a*c
+        
+        time=0
 
-    ai.turn(diffAngle)
-    return diffAngle
-
-def timeOfImpact(relativeX, relativeY, targetSpeedX, targetSpeedY, bulletSpeed): #inspired by http://playtechs.blogspot.se/2007/04/aiming-at-moving-target.html
-
-    a=bulletSpeed * bulletSpeed - (targetSpeedX*targetSpeedX+targetSpeedY*targetSpeedY)
-    b=relativeX*targetSpeedX+relativeY*targetSpeedY
-    c=relativeX*relativeX+relativeY*relativeY
-
-    d=b*b+a*c
-    
-    time=0
-
-    if d >= 0:
-        time = ( b + math.sqrt(d) ) /a
+        if d >= 0:
+            time = ( b + math.sqrt(d) ) /a
         if time < 0:
             time = 0
 
-    return time
+        return time
 	
-def flyTo(targetX,targetY,selfX,selfY):
-    targetAngle=math.atan2(targetY-selfY,targetX-selfX)
-    targetAngle=ai.radToDeg(targetAngle)
-    egenAngle=int(ai.selfHeadingDeg())
-    diffAngle=ai.angleDiff(egenAngle,targetAngle)
-    diffX=selfX-targetX
-    diffY=selfY-targetY
+    def flyTo(targetX,targetY,selfX,selfY):
+        targetAngle=math.atan2(targetY-selfY,targetX-selfX)
+        targetAngle=ai.radToDeg(targetAngle)
+        egenAngle=int(ai.selfHeadingDeg())
+        diffAngle=ai.angleDiff(egenAngle,targetAngle)
+        diffX=selfX-targetX
+        diffY=selfY-targetY
 
-    ai.turn(diffAngle)
-    if ai.closestShipId() != -1:
-        print("stopping")
-        ai.thrust(0)
-        return
+        return(diffAngle)
 
-    if ai.selfSpeed() < 2:
-        ai.thrust(1)
-    else:
-        ai.thrust(0)
-
-def turnAngle(heading):
-    heading += 180
-    heading %= 360
-    return(heading)
 
  
 #
@@ -232,7 +221,7 @@ def AI_loop():
 (options, args) = parser.parse_args()
 
 port = 15345 + options.group
-name = "Stub"
+name = "Voldemort"
 
 #
 # Start the main loop. Callback are done to AI_loop.
