a541a2f07fac4bbfc1f47d7282ffd0a3fa1ecb60
John Litborn <johli603@student.liu.se>
2013-01-07 18:05:55 +0100

    Doesn't directly aim towards where we want to go, instead figures out where we should thrust in order to go in the wanted direction, taking account in which direction we're already heading. Also some other changes ... like tons

--- finalAI.py
+++ finalAI.py
@@ -3,6 +3,7 @@ import libpyAI as ai
 import random
 import sys
 import math
+import time ###TODO: remove this when done testing
 
 from optparse import OptionParser
 
@@ -21,7 +22,6 @@ class myai:
     #
     def __init__(self):
         self.count = 0
-        self.wantedMaximalSpeed = 9999
         self.wantedHeading = 90
         self.mode = "init"
         random.seed()
@@ -44,6 +44,16 @@ class myai:
 
             self.count += 1
 
+            #
+            # Constants
+            #
+            ai.setTurnSpeed(32)
+            bulletVel=21
+            shootDistance=50
+            mapSize=2240 ##Assumes it's a square
+            radarSize=256 ##Same on all maps
+            radarToScreen=mapSize/radarSize
+            thrust=False
 
             #
             # Read the ships sensors.
@@ -57,50 +67,61 @@ class myai:
             selfVelX = ai.selfVelX()
             selfVelY = ai.selfVelY()
             selfVel = ai.selfSpeed()
+
             selfTracking = ai.selfTrackingDeg()
             selfHeading = ai.selfHeadingDeg()
+  
             
             #enemy readings
             enemyRadarX=ai.closestRadarX()
             enemyRadarY=ai.closestRadarY()
+            if enemyRadarX == -1:
+                enemyExists=False
+            else:
+                enemyExists=True
             closestShip = ai.closestShipId()
             if closestShip != -1:
                 enemyX=ai.screenEnemyXId(closestShip)
                 enemyY=ai.screenEnemyYId(closestShip)
                 enemyVel=ai.enemySpeedId(closestShip)
                 enemyTracking=ai.enemyTrackingRadId(closestShip)
-                if enemyTracking == None or math.isnan(enemyTracking):
-                    enemyTracking=0
-                enemyVelX=enemyVel*math.cos(enemyTracking)
-                enemyVelY=enemyVel*math.sin(enemyTracking)
-                closestEnemyScreen=ClosestEnemy(closestShip, selfX, selfY, enemyX, enemyY, 2240)
-                enemyX=closestEnemyScreen[0]
-                enemyY=closestEnemyScreen[1]
+                if enemyTracking==None:
+                    enemyVelX=0
+                    enemyVelY=0
+                elif math.isnan(enemyTracking):
+                    enemyTracking=None
+                    enemyVelX=0
+                    enemyVelY=0
+                else:
+                    enemyVelX=enemyVel*math.cos(enemyTracking)
+                    enemyVelY=enemyVel*math.sin(enemyTracking)
+                (enemyX, enemyY)=ClosestEnemy(selfX, selfY, enemyX, enemyY, mapSize)
+#                closestEnemyScreen=ClosestEnemy(selfX, selfY, enemyX, enemyY, mapSize)
+#                enemyX=closestEnemyScreen[0]
+#                enemyY=closestEnemyScreen[1]
             #
             # Done reading sensors
             #
 
 
             
-            
-            #
-            # Constants
-            #
-            ai.setTurnSpeed(32)
-            bulletVel=21
             #
             # adjust sensor readings
             #
             if math.isnan(selfTracking): #cause nan is a bitch
-                selfTracking=0
-            checkDist=int(selfVel*30)
-            if checkDist < 100:
-                checkDist = 100
+                selfTracking=None
+            checkDist=int(selfVel*40)
+            if checkDist < 150:
+                checkDist = 150
             
             # Fix radar readings so we go the shortest way to people when they are on the other side of the edge of the map
-            closestEnemyRadar=ClosestEnemy(selfRadarX, selfRadarY, enemyRadarX, enemyRadarY, 256)
-            enemyRadarX=closestEnemyRadar[0]
-            enemyradarY=closestEnemyRadar[1]
+            (enemyradarX, enemyRadarY)=ClosestEnemy(selfRadarX, selfRadarY, enemyRadarX, enemyRadarY, radarSize)
+#            closestEnemyRadar=ClosestEnemy(selfRadarX, selfRadarY, enemyRadarX, enemyRadarY, radarSize)
+#            enemyRadarX=closestEnemyRadar[0]
+#            enemyradarY=closestEnemyRadar[1]
+            enemyRadarDistance=Distance(selfRadarX, selfRadarY, enemyRadarX, enemyRadarY)
+            if not enemyExists:
+                enemyRadarDistance=99999 #arbitrarily high number
             
         except:
             e = sys.exc_info()
@@ -115,44 +136,62 @@ class myai:
             # At all times we want to check if we are crashing into anything, unless we are already avoiding it
             if self.mode != "thrust" and CheckWall(checkDist, selfTracking):
                 self.wantedDirection, self.wantedHeading = AvoidCrash(checkDist, selfTracking, self.wantedDirection)
+                crashing=True
                 self.mode = "turn"
+            else:
+                crashing=False
             
             
             # If we are close to being hit, try and avoid (Seldom works at final.xp's settings, but it's worth a try!)
             if self.mode != "turn" and Danger(selfX, selfY, selfVelX, selfVelY) != False:
                 self.mode = "dodge"
-            
             #
             # Start of state machine part
             #
 #################################################
+            start=time.time()
             if self.count == 2 and self.mode != "thrust":
                 self.mode = "move"
 
 #################################################
+            elif self.mode == "wait":
+                thrust=False ###TODO: For some reason this doesn't go through, after killing an enemy it thrusts like crazy. Bug in API?
+                ai.setPower(5)
+                if enemyExists:
+                    self.mode = "move"
+#################################################
             elif self.mode == "move":
-                if closestShip != -1:
+                if enemyRadarDistance < shootDistance:
                     self.mode = "shoot"
                     return
-                
-                ai.fireShot()
+                if not enemyExists:
+                    self.mode = "wait"
+                    return
+
+                #ai.fireShot() ######weeeelll, this was said to be ugly
                 self.wantedHeading=FlyTo(enemyRadarX, enemyRadarY,selfRadarX,selfRadarY)
 
                 # Adjust course if we want to head into a wall
                 self.wantedHeading=AdjustCourse(checkDist, self.wantedHeading)
+                #print(self.wantedHeading, selfTracking, CounteractTracking(self.wantedHeading, selfTracking))
+                self.wantedHeading=CounteractTracking(self.wantedHeading, selfTracking)
 
                 TurnToAngle(selfHeading, self.wantedHeading)
-                if selfVel < self.wantedMaximalSpeed and ai.angleDiff(int(selfHeading), int(self.wantedHeading)) < 90:
-                    ai.thrust(1)
+                AdjustPower(enemyRadarDistance)
+                if selfVel < ai.angleDiff(int(selfHeading), int(self.wantedHeading)) < 90:
+                    thrust=True
                 else:
-                    ai.thrust(0)
+                    thrust=False
+
 
 #################################################
-            elif self.mode == "dodge":
+            elif self.mode == "dodge": ##TODO: rewrite
                 incAngle=Danger(selfX, selfY, selfVelX, selfVelY)
+                if selfTracking == None:
+                    return
                 if ai.angleDiff(int(incAngle), int(selfTracking+180)) < 5: ##TODO: Works really bad
-                    print("Changing angle not to charge into shot")
-                    self.wantedHeading=selfTracking+45
+                    #print("Changing angle not to charge into shot")
+                    self.wantedHeading=selfTracking+45 ###TODO doesn't work when selfTracking==None
                     self.wantedHeading=AdjustCourse(checkDist, self.wantedHeading)
                     TurnToAngle(selfHeading, self.wantedHeading)
 
@@ -161,34 +200,66 @@ class myai:
 
 #################################################
             elif self.mode == "turn":
-                ai.thrust(0)
+                if crashing == False:
+                    self.mode="move"
                 self.wantedHeading=AdjustCourse(checkDist, self.wantedHeading) ##TODO: Check if this is needed or so
+                self.wantedHeading=CounteractTracking(self.wantedHeading, selfTracking)
                 TurnToAngle(selfHeading, self.wantedHeading)
                 
+                #AdjustPower(enemyRadarDistance, distanceToWall) ##TODO: Do some math on trackingVelocity and distance to wall and adjust power accordingly
+                ai.setPower(55)
+                
                 if abs(ai.angleDiff (int(selfHeading), int(self.wantedHeading))) < 10:
-                    self.ticksLeftToThrust=5
-                    self.mode = "thrust"
+                    thrust=True
+                else:
+                    thrust=False
 
 #################################################
             elif self.mode == "thrust":
                 
                 if self.ticksLeftToThrust > 0:
-                    ai.thrust(1)
+                    ai.setPower(55)
+                    #print("THRUUUUSTING")
+                    thrust=True
                     self.ticksLeftToThrust -= 1
                 else:
                     self.wantedDirection = ""
                     self.mode = "move"
-                    ai.thrust(0)
+                    thrust=False
                 
 #################################################
             elif self.mode == "shoot":
                 self.wantedHeading=0
-                ai.thrust(0)
-                if closestShip == -1:
+                if enemyRadarDistance > shootDistance:
                     self.mode = "move"
+	if closestShip == -1:
+                    self.wantedHeading=FlyTo(enemyRadarX, enemyRadarY,selfRadarX,selfRadarY)
                 else:
-                    TurnToAngle(selfHeading, Shoot(closestShip, selfX, selfY, selfVelX, selfVelY, enemyX, enemyY, enemyVelX, enemyVelY, enemyTracking, bulletVel))
-                    ai.fireShot()
+                    self.wantedHeading=Shoot(closestShip, selfX, selfY, selfVelX, selfVelY, enemyX, enemyY, enemyVelX, enemyVelY, enemyTracking, bulletVel)
+
+
+                if CheckWall(enemyRadarDistance*radarToScreen, self.wantedHeading): ##Check if there's a wall in the way
+                    self.wantedHeading=(self.wantedHeading+90)%360
+                    self.ticksLeftToThrust = 1
+                    self.mode="thrust"
+                    return
+                
+                TurnToAngle(selfHeading, self.wantedHeading)
+                ai.fireShot()
+                ##Counteract recoil
+                ai.setPower(8)
+                thrust=True
+            #print(thrust)
+            timeTaken=(time.time() - start)
+            if timeTaken > 0.01:
+                #print(self.count, self.mode, timeTaken)
+                pass
+            if thrust:
+                ai.thrust(0)
+                ai.thrust(1)
+            else:
+                ai.thrust(1)
+                ai.thrust(0)
 #################################################
             #
             # End of state machine part
@@ -203,7 +274,7 @@ def AdjustCourse(checkDist, wantedHeading):
         i=0
         degreeChange=20
         distCurrent=CheckWall(checkDist, wantedHeading)
-        while distCurrent and i < 2:
+        while distCurrent and i < 4:
             distPositive=CheckWall(checkDist, wantedHeading+degreeChange)
             distNegative=CheckWall(checkDist, wantedHeading-degreeChange)
             lastOperation="plus"
@@ -291,6 +362,37 @@ def Shoot(id, selfX, selfY, selfVelX, selfVelY, enemyX, enemyY, enemyVelX, enemy
         e = sys.exc_info()
         print ("ERROR in function Shoot: ", e)
 
+#Calculates in which direction to thrust to get to the wanted heading, compensating
+def CounteractTracking (heading, tracking):
+    try:
+        if tracking==None:
+            return heading
+        heading=heading*math.pi/180 ##degrees->radians
+        tracking=tracking*math.pi/180 ##degrees->radians
+        resultMatrix=[math.cos(heading)-math.cos(tracking), math.sin(heading)-math.sin(tracking)]
+        length=math.sqrt((resultMatrix[0]**2+resultMatrix[1]**2))
+        if length==0:
+            return heading
+        resultMatrix[0]/=length
+        resultMatrix[1]/=length
+        a=math.acos(resultMatrix[0])
+        b=-a
+        c=math.asin(resultMatrix[1])
+        d=c+math.pi/2
+
+        for x in a,b:
+            for y in c,d:
+                if abs(x-y)<0.01:
+                    if abs(a-c)>0.01 and abs(a-d)>0.01 and abs(b-c)>0.01:
+                        print(a,b,c,d)
+                    return x*180/math.pi ## radians->degrees
+
+        #print(heading, tracking, a,b,c,d,"fail") ##Debugging
+        return heading*180/math.pi
+    except:
+        e= sys.exc_info()
+        print ("Error in function CounteractTracking: ", e)
+
 #Creates clones of the closest enemy so we can track him through the edges of the map
 def ClosestEnemy(selfX, selfY, enemyX, enemyY, mapConstant):
     try:
@@ -344,7 +446,7 @@ def CheckWall(dist, direction):
         if not dist or not direction:
             return False
         distance_to_wall = ai.wallFeeler(int(dist), int(direction), 1, 1)
-        if dist == distance_to_wall:
+        if int(dist) == distance_to_wall:
             return False
         else:
             return distance_to_wall
@@ -367,7 +469,16 @@ def FlyTo(targetX,targetY,selfX,selfY):
 def Distance(x0, y0, x1, y1):
     return math.sqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0))
 
-
+def AdjustPower(dist):
+    if dist < 50:
+        ai.setPower(5)
+    elif dist < 100:
+        ai.setPower(20)
+    elif dist < 150:
+        ai.setPower(35)
+    else:
+        ai.setPower(55)
+    return
 
 
 bot = myai()
@@ -385,4 +496,4 @@ name = random.randint(1, 999999)
 
 # The command line arguments to xpilot can be given in the list in the second argument
 # 
-ai.start(AI_loop,["-name", name, "-join", "-fuelMeter", "yes", "-showHUD", "no", "-port", str(port)])
+ai.start(AI_loop,[])#"-name", name, "-join", "-fuelMeter", "yes", "-showHUD", "no", "-port", str(port)])
